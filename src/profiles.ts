f868e8a1eaf663d210e32b7d60d65dfb379d47f0
Ankit <iankit17@gmail.com>
2022-12-01 21:05:57 +0530

    update condition

--- src/profiles.ts
+++ src/profiles.ts
@@ -2,8 +2,7 @@ import {
   DisableProfile as DisableProfileEvent,
   NewProfileCreated as NewProfileCreatedEvent,
 } from "../generated/Profiles/Profiles";
-import { Address, ipfs, json } from "@graphprotocol/graph-ts";
-import { integer } from "@protofire/subgraph-toolkit";
+import { Address, ipfs, json, log } from "@graphprotocol/graph-ts";
 import { Account, Profile } from "../generated/schema";
 
 export function handleDisableEvent(event: DisableProfileEvent): void {
@@ -22,91 +21,90 @@ export function handleNewProfileCreated(event: NewProfileCreatedEvent): void {
     newProfile.profileID = event.params.profileID;
     newProfile.profileDataCID = event.params.profileDataCID;
     newProfile.creatorAddress = event.params.creatorAddress;
-    // newProfile.eventTimestamp = event.params.eventTimestamp;
     newProfile.isDisabled = event.params.isDisabled;
     newProfile.blockNumber = event.block.number;
     newProfile.blockTimestamp = event.block.timestamp;
     newProfile.transactionHash = event.transaction.hash;
 
-    let metadata = ipfs.cat(event.params.profileDataCID + "/data.json");
+    if (event.params.profileDataCID) {
+      let metadata = ipfs.cat(event.params.profileDataCID + "/data.json");
+      log.info("xoxox: {}", [event.params.profileDataCID.toString()]);
 
-    if (metadata) {
-      const value = json.fromBytes(metadata).toObject();
-      if (value) {
-        const firstName = value.get("firstName");
-        const jobTitle = value.get("jobTitle");
-        const city = value.get("city");
-        const country = value.get("country");
-        const website = value.get("website");
-        const facebookPage = value.get("facebookPage");
-        const facebookGroup = value.get("facebookGroup");
-        const twitter = value.get("twitter");
-        const instagram = value.get("instagram");
-        const pinterest = value.get("pinterest");
-        const tiktok = value.get("tiktok");
-        const linkedin = value.get("linkedin");
-        const freebie = value.get("freebie");
-        const otherLink = value.get("otherLink");
-        const bio = value.get("bio");
-        const imagePath = value.get("coverImage");
+      if (metadata) {
+        const value = json.fromBytes(metadata).toObject();
+        if (value) {
+          const firstName = value.get("name");
+          const jobTitle = value.get("job");
+          const city = value.get("city");
+          const country = value.get("country");
+          const website = value.get("website");
+          const facebookPage = value.get("facebookPage");
+          const facebookGroup = value.get("facebookGroup");
+          const twitter = value.get("twitter");
+          const instagram = value.get("instagram");
+          const pinterest = value.get("pinterest");
+          const tiktok = value.get("tiktok");
+          const linkedin = value.get("linkedin");
+          const freebie = value.get("freebie");
+          const otherLink = value.get("otherLink");
+          const bio = value.get("bio");
+          const imagePath = value.get("image");
 
-        if (firstName) {
-          newProfile.firstName = firstName.toString();
-        }
-        if (jobTitle) {
-          newProfile.jobTitle = jobTitle.toString();
-        }
-        if (city) {
-          newProfile.city = city.toString();
-        }
-        if (country) {
-          newProfile.country = country.toString();
-        }
-        if (website) {
-          newProfile.website = website.toString();
-        }
-        if (facebookPage) {
-          newProfile.facebookPage = facebookPage.toString();
-        }
-        if (facebookGroup) {
-          newProfile.facebookGroup = facebookGroup.toString();
-        }
-        if (twitter) {
-          newProfile.twitter = twitter.toString();
-        }
-        if (instagram) {
-          newProfile.instagram = instagram.toString();
-        }
-        if (pinterest) {
-          newProfile.pinterest = pinterest.toString();
-        }
-        if (tiktok) {
-          newProfile.tiktok = tiktok.toString();
-        }
-        if (linkedin) {
-          newProfile.linkedin = linkedin.toString();
-        }
-        if (freebie) {
-          newProfile.freebie = freebie.toString();
-        }
-        if (otherLink) {
-          newProfile.otherLink = otherLink.toString();
-        }
-        if (bio) {
-          newProfile.bio = bio.toString();
-        }
+          if (firstName) {
+            newProfile.firstName = firstName.toString();
+          }
+          if (jobTitle) {
+            newProfile.jobTitle = jobTitle.toString();
+          }
+          if (city) {
+            newProfile.city = city.toString();
+          }
+          if (country) {
+            newProfile.country = country.toString();
+          }
+          if (website) {
+            newProfile.website = website.toString();
+          }
+          if (facebookPage) {
+            newProfile.facebookPage = facebookPage.toString();
+          }
+          if (facebookGroup) {
+            newProfile.facebookGroup = facebookGroup.toString();
+          }
+          if (twitter) {
+            newProfile.twitter = twitter.toString();
+          }
+          if (instagram) {
+            newProfile.instagram = instagram.toString();
+          }
+          if (pinterest) {
+            newProfile.pinterest = pinterest.toString();
+          }
+          if (tiktok) {
+            newProfile.tiktok = tiktok.toString();
+          }
+          if (linkedin) {
+            newProfile.linkedin = linkedin.toString();
+          }
+          if (freebie) {
+            newProfile.freebie = freebie.toString();
+          }
+          if (otherLink) {
+            newProfile.otherLink = otherLink.toString();
+          }
+          if (bio) {
+            newProfile.bio = bio.toString();
+          }
 
-        if (imagePath) {
-          const imageURL =
-            "https://ipfs.io/ipfs/" +
-            event.params.profileDataCID +
-            imagePath.toString();
-          newProfile.imageURL = imageURL;
-        } else {
-          // return fallback image if no imagePath
-          const fallbackURL =
-            "https://ipfs.io/ipfs/bafybeibssbrlptcefbqfh4vpw2wlmqfj2kgxt3nil4yujxbmdznau3t5wi/event.png";
-          newProfile.imageURL = fallbackURL;
+          if (imagePath) {
+            const imageURL = "https://ipfs.io/ipfs/" + event.params.profileDataCID + imagePath.toString();
+            newProfile.imageURL = imageURL;
+          } else {
+            // return fallback image if no imagePath
+            const fallbackURL =
+              "https://ipfs.io/ipfs/bafybeibssbrlptcefbqfh4vpw2wlmqfj2kgxt3nil4yujxbmdznau3t5wi/event.png";
+            newProfile.imageURL = fallbackURL;
+          }
         }
       }
     }
